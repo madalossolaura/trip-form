0f790f14f2a6d4b6bfc3c3cb80127198cfa364a2
Laura Madalosso <madalossolaura@gmail.com>
2021-08-16 16:20:45 -0300

    start project

---
+++ components/Form/Form.js
@@ -0,0 +1,10 @@
+export default function Form() {
+    return (
+        <form>
+            <div>
+                <label>label</label>
+                <input/>
+            </div>
+        </form>
+    );
+}  
